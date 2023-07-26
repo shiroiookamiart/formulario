03dd24505efd9d39ef5ece4e70243ecd93e32e3d
dbeltran <dbeltran486@gmail.com>
2023-07-26 08:57:11 -0400

    modificaciones

--- src/app/peliculas/peliculas.component.ts
+++ src/app/peliculas/peliculas.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //se importa el angular
 
-@Component({
+@Component({ //se agrega informacion de mi ciomponente peliculas
   selector: 'app-peliculas',
   templateUrl: './peliculas.component.html',
   styleUrls: ['./peliculas.component.css']
 })
 
-export class PeliculasComponent {
+export class PeliculasComponent { //creo mi componete usando typescript
   nombrePelicula = '';
   peliculas: any = [];
 
-  agregarPelicula() {
+  agregarPelicula() { //metodo para el alacenamineto de mi pelicula
     this.peliculas.push(this.nombrePelicula);
     this.nombrePelicula = '';
   }
